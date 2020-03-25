a51e6b21400b3e022455fe2cd74ad7bd02b304b3
Sijin Woo <sijin.woo@utexas.edu>
2020-03-25 02:31:44 -0500

    Adds hid task priorities and stack sizes.

--- firmware/cfg/inc/app_cfg.h
+++ firmware/cfg/inc/app_cfg.h
@@ -28,9 +28,12 @@
 #define CLK_OS_CFG_TASK_PRIO                3u
 #define USBD_OS_CFG_CORE_TASK_PRIO          4u
 #define USBD_HID_OS_CFG_TMR_TASK_PRIO       5u
+#define APP_CFG_USBD_HID_WRITE_TASK_PRIO    6u
+#define APP_CFG_USBD_HID_READ_TASK_PRIO     7u
 
 #define CLK_OS_CFG_TASK_STK_SIZE            256u
 #define USBD_HID_OS_CFG_TMR_TASK_STK_SIZE   512u
-#define USBD_OS_CFG_CORE_TASK_STK_SIZE      512u
+#define USBD_OS_CFG_CORE_TASK_STK_SIZE      1024u
+#define APP_CFG_USBD_HID_TASK_STK_SIZE      256u
 
 #endif
