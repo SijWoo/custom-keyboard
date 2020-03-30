ec12b294c66177bd949dd83abcc53ed8d0aa5716
Sijin Woo <sijin.woo@utexas.edu>
2020-03-29 22:56:52 -0500

    Adds keyboard_cfg to easily modify the layout. Adds Gesture_Init prototype.

---
+++ firmware/cfg/inc/keyboard_cfg.h
@@ -0,0 +1,6 @@
+#ifndef __KEYBOARD_CFG_H
+#define __KEYBOARD_CFG_H
+
+
+
+#endif
--- firmware/drivers/gesture/inc/gesture.h
+++ firmware/drivers/gesture/inc/gesture.h
@@ -1,6 +1,13 @@
 #ifndef __GESTURE_H
 #define __GESTURE_H
 
+/**
+ * @brief   Initializes the Gesture sensors and MUX.
+ * @param   None
+ * @return  None
+ */
+void Gesture_Init(void);
+
 
 
 #endif
